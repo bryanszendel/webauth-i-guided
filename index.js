1f701fffbaec5ce64ab87178cae29560652875f3
Bryan Szendel <bryan.szendel@gmail.com>
2019-09-16 11:45:51 -0600

    added userChecker middleware

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', userChecker, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -73,3 +73,32 @@ server.get('/api/hash', (req, res) => {
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+
+/*
+write a middleware that will check for the username and password and let the request continue to /api/users if credentials are good
+return a 401 if the credentials are invalid
+
+use the middleware to retrict access to GET /api/users endpoint
+
+*/
+
+function userChecker(req, res, next) {
+  const { username, password } = req.headers
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials.'})
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Unexpected error.'})
+      })
+  } else {
+    res.status(400).json({ message: 'No credentials provided.'})
+  }
+}
